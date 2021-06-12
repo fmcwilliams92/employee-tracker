7a4a563608d41783d03835a03f3b8965bc11d1ef
Fredric McWilliams <fredmcwilliams92@gmail.com>
2021-06-11 21:16:42 -0500

    fixed prompt questions giving the answers a value

--- index.js
+++ index.js
@@ -7,17 +7,38 @@ function questions() {
       name: 'options',
       type: 'list',
       choices: [
-
-        {name: 'View all Departments',
-        value:"VIEW_ALL_DEPARTMENTS"
-      },
-        {'View all Roles'},
-        {'View all Employees'},
-        {'Add a Department'},
-        {'Add a Role'},
-        {'Ann an Employee'},
-        {'Update an Employee Role'},
-        {'Exit'}
+        {
+          name: 'View all Departments',
+          value:"VIEW_ALL_DEPARTMENTS"
+        },
+        {
+          name: 'View all Roles',
+          value: 'VIEW_ALL_ROLES'
+        },
+        {
+          name: 'View all Employees',
+          value: 'VIEW_ALL_EMPLOYEES'
+        },
+        {
+          name: 'Add a Department',
+          value: 'ADD_A_DEPARTMENT'
+        },
+        {
+          name: 'Add a Role',
+          value: 'ADD_A_ROLE'
+        },
+        {
+          name: 'Add an Employee',
+          value: 'ADD_AN_EMPLOYEE'
+        },
+        {
+          name: 'Update an Employee Role',
+          value: 'UPDATE_AN_EMPLOYEE'
+        },
+        {
+          name: 'Exit',
+          value: 'EXIT'
+        }
       ]
     }
   ])
